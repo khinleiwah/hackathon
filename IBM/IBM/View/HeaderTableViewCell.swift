3c8090e25db3fcf55d366537697fce06803192bc
Khin Lei Wah Win <khinleiwahwin84@gmail.com>
2019-04-06 13:11:02 +0800

    custom resuable table view cell

---
+++ IBM/IBM/View/HeaderTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  HeaderTableViewCell.swift
+//  IBM
+//
+//  Created by Win on 6/4/19.
+//  Copyright © 2019 Win. All rights reserved.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
