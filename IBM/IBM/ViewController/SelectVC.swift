24e68547dfec884aab14712c629f57bcbd225ea6
Liu Fan Wei <>
2019-04-07 00:17:13 +0800

    fix merge code case code missing issue

--- IBM/IBM/ViewController/SelectVC.swift
+++ IBM/IBM/ViewController/SelectVC.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class SelectVC: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(UINib.init(nibName: "HeaderTableViewCell", bundle: nil), forHeaderFooterViewReuseIdentifier: "HeaderTableViewCell")
+        tableView.register(UINib.init(nibName: "HeaderTableViewCell", bundle: nil), forCellReuseIdentifier: "HeaderTableViewCell")
     }
 }
 extension SelectVC: UITableViewDelegate, UITableViewDataSource{
@@ -24,16 +24,12 @@ extension SelectVC: UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
-            return tableView.dequeueReusableCell(withIdentifier: "HeaderTableViewCell", for: indexPath)
-        }else if indexPath.row == 1 {
-            let filterTableViewCell = tableView.dequeueReusableCell(withIdentifier: "HeaderTableViewCell", for: indexPath) as! HeaderTableViewCell
-            return filterTableViewCell
-        }
-        return UITableViewCell()
+        let filterTableViewCell = tableView.dequeueReusableCell(withIdentifier: "HeaderTableViewCell", for: indexPath) as! HeaderTableViewCell
+        return filterTableViewCell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
+        dismiss(animated: true, completion: nil)
     }
 }
