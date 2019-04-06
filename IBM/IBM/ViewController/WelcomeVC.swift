84d8b6b19c3ed1462a5f16cd6176755fc4eea8e1
Liu Fan Wei <>
2019-04-06 13:01:51 +0800

    add mock data manager and welcome page

---
+++ IBM/IBM/ViewController/WelcomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  HistoryVC.swift
+//  IBM
+//
+//  Created by Liu Fan Wei on 6/4/19.
+//  Copyright © 2019 Win. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        labelTitle.text = "Welcome " + DataManager.shared.name
+    }
+}
