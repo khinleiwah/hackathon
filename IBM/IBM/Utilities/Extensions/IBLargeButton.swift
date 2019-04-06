bf515891a3ea474ed8a5624ab6b94c5a6c1b3efe
Khin Lei Wah Win <khinleiwahwin84@gmail.com>
2019-04-06 16:30:27 +0800

    optimized method for transactions

---
+++ IBM/IBM/Utilities/Extensions/IBLargeButton.swift
@@ -0,0 +1,21 @@
+//
+//  IBLargeButton.swift
+//  IBM
+//
+//  Created by Kasun Ranasinghe on 4/6/19.
+//  Copyright © 2019 Win. All rights reserved.
+//
+
+import UIKit
+
+class IBLargeButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
